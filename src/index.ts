69c8573fcbaaeecfd9d907ca84c5e2d3a6087be4
aabccd021 <aabccd021@gmail.com>
2025-05-31 12:54:48 +0700

    Refactor import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { handleQueueRequest } from "./queue.ts";
 
 // TODO: concurrency
 // TODO: chunk 2000
-// TODO: select message from least recently processed ratelimit bucket
 // TODO; journald compatible logging
 
 process.on("uncaughtException", (error) => {
