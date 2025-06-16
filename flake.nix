4c2d11c14053a06b12feb09ac64bad57ac74884b
aabccd021 <aabccd021@gmail.com>
2025-06-16 07:50:32 +0700

    feat: Add function to collect inputs in flake.nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,19 @@
   outputs =
     { self, ... }@inputs:
     let
+      lib = inputs.nixpkgs.lib;
+
+      collectInputs =
+        is:
+        pkgs.linkFarm "inputs" (
+          builtins.mapAttrs (
+            name: i:
+            pkgs.linkFarm name {
+              self = i.outPath;
+              deps = collectInputs (lib.attrByPath [ "inputs" ] { } i);
+            }
+          ) is
+        );
 
       overlays.default = (
         final: prev:
@@ -100,6 +113,7 @@
           tests = pkgs.linkFarm "tests" test;
           formatting = treefmtEval.config.build.check self;
           formatter = formatter;
+          allInputs = collectInputs inputs;
           typeCheck = typeCheck;
           lintCheck = lintCheck;
         };
