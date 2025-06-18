ac0399baf5ec291455a1e674e22d710765c2f50b
aabccd021 <aabccd021@gmail.com>
2025-06-18 11:16:09 +0700

    refactor: optimize Discord webhook service configuration

--- nixosModules.nix
+++ nixosModules.nix
@@ -1,49 +1,92 @@
 { inputs }:
-{ lib, ... }:
 {
-  options.services.senddiscord = {
-    enable = lib.mkEnableOption "Discord sendmail service";
-    webhookUrl = lib.mkOption {
-      type = lib.types.str;
-      description = "The Discord webhook URL to send messages to.";
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+let
+  cfg = config.services.send-discord;
+
+  server = import ./package.nix {
+    pkgs = pkgs;
+    inputs = inputs;
+  };
+in
+
+{
+
+  options.services.send-discord = {
+    enable = lib.mkEnableOption "Discord Sendmail Service";
+    webhookUrlFile = lib.mkOption {
+      type = lib.types.path;
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 14000;
+      description = "Port for the Discord webhook dispatcher service.";
     };
   };
 
-  nixpkgs.overlays = [
-    (
-      final: prev:
-      let
-        package = import ./package.nix {
-          pkgs = final;
-          inputs = inputs;
-        };
-      in
-      {
-        senddiscord = package;
+  config = lib.mkIf cfg.enable {
+    nixpkgs.overlays = [
+      (final: prev: {
         sendmail = final.writeShellApplication {
           name = "sendmail";
           runtimeInputs = [
-            final.curl
             final.jq
+            final.curl
           ];
+          runtimeEnv.WEBHOOK_URL_FILE = cfg.webhookUrlFile;
+          runtimeEnv.PORT = toString cfg.port;
           text = ''
+            set -x
             stdin=$(cat)
-            content=$(echo "$stdin" | jq -R -s .)
+            if [ -z "$stdin" ]; then
+              echo "No input provided. Exiting."
+              exit 0
+            fi
+            content=$(printf "%s" "$stdin" | jq --raw-input --slurp --raw-output '@json')
+            webhook_url=$(cat "$WEBHOOK_URL_FILE")
             exec curl \
               --request POST \
-              --url http://localhost:13000/ \
-                            --silent \
+              --url "http://127.0.0.1:$PORT" \
+              --silent \
               --show-error \
               --fail \
               --header 'Content-Type: application/json' \
-              --header 'X-Discord-Webhook-Url: http://localhost:3001' \
-              --data "{
-                \"content\": $content
-              }"
+              --header "X-Discord-Webhook-Url: $webhook_url" \
+              --data "{ \"content\": $content }"
           '';
         };
-      }
-    )
-  ];
+      })
+    ];
+
+    environment.systemPackages = [
+      pkgs.sendmail
+    ];
+
+    services.mail.sendmailSetuidWrapper = {
+      program = "sendmail";
+      source = "${pkgs.sendmail}/bin/sendmail";
+      setuid = false;
+      setgid = false;
+      owner = "root";
+      group = "root";
+    };
+
+    systemd.services.discord-sendmail = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.Type = "notify";
+      environment.PORT = toString cfg.port;
+      script = ''
+        exec ${lib.getExe server} --port "$PORT"
+      '';
+    };
+    systemd.tmpfiles.rules = [
+      "d /var/lib/discord-webhook-dispatcher/message_db 0700 root root"
+    ];
+  };
 
 }
