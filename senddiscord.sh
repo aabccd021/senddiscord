26cc499c3facc625243ab2b18347332b5442e9c9
aabccd021 <aabccd021@gmail.com>
2025-09-23 06:08:57 +0700

    Update senddiscord.sh script permissions and message processing.

--- senddiscord.sh
+++ senddiscord.sh
@@ -9,8 +9,7 @@ if [ ! -f /var/lib/senddiscord/messages.txt ]; then
   touch /var/lib/senddiscord/messages.txt
 fi
 
-chmod 0777 /var/lib/senddiscord
-chmod 0666 /var/lib/senddiscord/messages.txt
+chmod -R u=rwX /var/lib/senddiscord
 
 running=1
 trap 'running=0' TERM
@@ -18,13 +17,17 @@ trap 'running=0' TERM
 while [ "$running" -eq 1 ]; do
   sleep 2
 
-  content=$(head -n 1 /var/lib/senddiscord/messages.txt || true)
-  if [ -z "$content" ]; then
+  file_lines=$(wc -l </var/lib/senddiscord/messages.txt)
+  if [ "$file_lines" -eq 0 ]; then
     sleep 10
     continue
   fi
 
-  content=$(echo "$content" | head -c 1900 | jq --raw-input --slurp --raw-output '@json')
+  content=$(
+    head -n 1 /var/lib/senddiscord/messages.txt |
+      head -c 1900 |
+      jq --raw-input --slurp --raw-output '@json'
+  )
 
   status=$(
     curl \
@@ -49,5 +52,6 @@ while [ "$running" -eq 1 ]; do
   if [ "$status" != "204" ]; then
     echo "Failed to send message, status: $status, content: $content" >&2
   fi
+
   sed -i '1d' /var/lib/senddiscord/messages.txt
 done
